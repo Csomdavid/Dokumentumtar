09fc1561bee73aeed40a706d42287f00e351b301
Csomdavid <113185222+Csomdavid@users.noreply.github.com>
2026-05-07 11:12:32 +0200

    Update urls.py

--- dokumentumtar/urls.py
+++ dokumentumtar/urls.py
@@ -8,15 +8,15 @@ urlpatterns = [
     path('audit-log/', views.audit_log_view, name='audit_log'),
     path('document/<int:doc_id>/', views.document_detail, name='document_detail'),
 
-    # --- ÚJ ARCHÍVUM ÚTVONALAK ---
+    # ÚJ ARCHÍVUM ÚTVONALAK
     path('archive/', views.archive_list, name='archive_list'),
     path('archive/<int:doc_id>/', views.document_archive, name='document_archive'),
 
-    # --- JOGOSULTSÁGOK ÚTVONALAI ---
+    # JOGOSULTSÁGOK ÚTVONALAI
     path('permissions/', views.permission_manager, name='permission_manager'),
     path('permissions/delete/<int:perm_id>/', views.permission_delete, name='permission_delete'),
 
-    # --- PROFIL ÉS JELSZÓCSERE ---
+    # PROFIL ÉS JELSZÓCSERE
     path('profile/', views.profile_view, name='profile'),
 
     path('admin-password-reset/<int:user_id>/', views.admin_password_reset, name='admin_password_reset'),
